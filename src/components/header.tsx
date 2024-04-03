d3776dd4a93397316e34be7a3546f0e9e4136a8f
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2024-04-02 23:38:11 -0300

    feat: breakdown components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
 import Logo from '../assets/logo.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
   return (
-    <div className='flex items-center gap-5 p-2'>
+    <div className="flex items-center gap-5 p-2">
       <img src={Logo} alt="logo-nwl" />
 
-      <nav className='flex items-center gap-5'>
-        <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
-        <a href="" className="font-medium text-sm">Participantes</a>
+      <nav className="flex items-center gap-5">
+        <NavLink href={'/eventos'}>Eventos</NavLink>
+        <NavLink href={'/participantes'}>Participantes</NavLink>
       </nav>
     </div>
   )
